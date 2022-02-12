67533126d86099be4d8a0f0b797a339488a3297d
WilLevesque <60223836+WilLevesque@users.noreply.github.com>
2022-02-12 14:10:28 -0500

    merge

--- server/app/services/seller.profile.service.ts
+++ server/app/services/seller.profile.service.ts
@@ -19,35 +19,35 @@ export class SellerProfileService {
     }
 
     async getFullName(id: string): Promise<string> {
-        const fullName = await this.collection.find({ _id: id }).project({ name: 1 }).toString();
+        const fullName = this.collection.find({ _id: id }).project({ name: 1 }).toString();
         return fullName;
     }
 
     async getDescription(id: string): Promise<string> {
-        const description = await this.collection.find({ _id: id }).project({ description: 1 }).toString();
+        const description = this.collection.find({ _id: id }).project({ description: 1 }).toString();
         return description;
     }
 
     // TODO Object Products[]
     async getProducts(id: string): Promise<string> {
-        const products = await this.collection.find({ _id: id }).project({ availableProducts: 1 }).toString();
+        const products = this.collection.find({ _id: id }).project({ availableProducts: 1 }).toString();
         return products;
     }
 
     // TODO Object Products[] // toArray()
     async getHistory(id: string): Promise<string> {
-        const products = await this.collection.find({ _id: id }).project({ productsHistory: 1 }).toString();
+        const products = this.collection.find({ _id: id }).project({ productsHistory: 1 }).toString();
         return products;
     }
 
     async getImageUrl(id: string): Promise<string> {
-        const imageUrl = await this.collection.find({ _id: id }).project({ imageUrl: 1 }).toString();
+        const imageUrl = this.collection.find({ _id: id }).project({ imageUrl: 1 }).toString();
         return imageUrl;
     }
 
     // TODO Object Reviews[] // toArray()
     async getReviews(id: string): Promise<string> {
-        const reviewList = await this.collection.find({ _id: id }).project({ reviews: 1 }).toString();
+        const reviewList = this.collection.find({ _id: id }).project({ reviews: 1 }).toString();
         return reviewList;
     }
 
