99d3618657b3f8e0b5ef9e4e0428fbde2bdfb062
Julie-anne <jaeggi.ja@gmail.com>
2022-02-12 14:42:17 -0500

    Basic requests products

---
+++ server/app/product-service/active-products.service.ts
@@ -0,0 +1,30 @@
+import { ACTIVE_PRODUCTS_COLLECTION } from '@app/database/constants';
+import { DatabaseService } from '@app/database/database.service';
+import { Product } from '@app/product-service/product.interface';
+import { Collection } from 'mongodb';
+import { Service } from 'typedi';
+
+@Service()
+export class ActiveProductService {
+    constructor(private dbService: DatabaseService) {}
+
+    get collection(): Collection<Product> {
+        return this.dbService.database.collection(ACTIVE_PRODUCTS_COLLECTION);
+    }
+
+    async getProducts(): Promise<Product[]> {
+        return await this.collection.find().toArray();
+    }
+
+    async addProduct(product: Product): Promise<void> {
+        await this.collection.insertOne({ name: product.name, price: product.price, quantity: product.quantity });
+    }
+
+    async deleteProduct(product: Product): Promise<boolean> {
+        const result = this.collection.deleteOne(product);
+        return (await result).deletedCount === 1;
+    }
+
+    // TODO: ADD FIELD SEARCH
+    // async getProduct(): Promise<Product> {}
+}
