e50427ff784299b07b8681f766e229631c96f6e7
XavierDupuis <xavier0978@hotmail.fr>
2022-02-12 15:52:47 -0500

    cart

--- client/src/app/services/cart-service/cart.service.ts
+++ client/src/app/services/cart-service/cart.service.ts
@@ -1,17 +1,34 @@
 import { Injectable } from '@angular/core';
 import { ProductAddedToCart } from '@app/components/main-page/products/modal-select-product/modal-select-product/modal-select-product.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class CartService {
     selectedProducts: ProductAddedToCart[] = [];
+    selectedProductsSubject: BehaviorSubject<ProductAddedToCart[]> = new BehaviorSubject<ProductAddedToCart[]>([]);
     constructor() {}
 
-    addProduct(product: ProductAddedToCart) {
-        if (!product) {
+    addProduct(productToAdd: ProductAddedToCart) {
+        if (!productToAdd) {
             return;
         }
-        this.selectedProducts.push(product);
+        this.selectedProducts.push(productToAdd);
+        this.selectedProductsSubject.next(this.selectedProducts);
+    }
+
+    removeProduct(productToRemove: ProductAddedToCart) {
+        if (!productToRemove) {
+            return;
+        }
+        const productIndex = this.selectedProducts.findIndex((product) => {
+            return (productToRemove = product);
+        });
+        if (productIndex === -1) {
+            return;
+        }
+        this.selectedProducts.splice(productIndex, 1);
+        this.selectedProductsSubject.next(this.selectedProducts);
     }
 }
