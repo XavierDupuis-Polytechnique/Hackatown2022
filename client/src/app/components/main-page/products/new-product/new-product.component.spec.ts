060d575b8c5ca1aabe98a992d0c5667c0849369f
XavierDupuis <xavier0978@hotmail.fr>
2022-02-12 21:11:05 -0500

    lint
    Co-Authored-By: WilLevesque <60223836+WilLevesque@users.noreply.github.com>

--- client/src/app/components/main-page/products/new-product/new-product.component.spec.ts
+++ client/src/app/components/main-page/products/new-product/new-product.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NewProductComponent } from './new-product.component';
 
 describe('NewProductComponent', () => {
-  let component: NewProductComponent;
-  let fixture: ComponentFixture<NewProductComponent>;
+    let component: NewProductComponent;
+    let fixture: ComponentFixture<NewProductComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NewProductComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [NewProductComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NewProductComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NewProductComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/components/main-page/products/new-product/new-product.component.ts
+++ client/src/app/components/main-page/products/new-product/new-product.component.ts
@@ -4,47 +4,47 @@ import { Product } from '@app/components/main-page/products/product-card/product
 import { ProductService } from '@app/services/product-service/product.service';
 
 @Component({
-  selector: 'app-new-product',
-  templateUrl: './new-product.component.html',
-  styleUrls: ['./new-product.component.scss'],
+    selector: 'app-new-product',
+    templateUrl: './new-product.component.html',
+    styleUrls: ['./new-product.component.scss'],
 })
 export class NewProductComponent implements OnInit {
-  nameAndDescription: FormGroup;
-  quantityAndPrice: FormGroup;
-  dates: FormGroup;
+    nameAndDescription: FormGroup;
+    quantityAndPrice: FormGroup;
+    dates: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private productService: ProductService) {}
+    constructor(private formBuilder: FormBuilder, private productService: ProductService) {}
 
-  ngOnInit(): void {
-    this.nameAndDescription = this.formBuilder.group({
-      name: ['', Validators.required],
-      description: ['', Validators.required],
-    });
-    this.quantityAndPrice = this.formBuilder.group({
-      quantity: [1, [Validators.required, Validators.min(1)]],
-      price: [0, [Validators.required, Validators.min(0)]],
-    });
-    this.dates = this.formBuilder.group({
-      productionDate: [new Date(), Validators.required],
-      maxPickupDate: [new Date(), [Validators.required, Validators.min(Date.now())]],
-    });
-  }
+    ngOnInit(): void {
+        this.nameAndDescription = this.formBuilder.group({
+            name: ['', Validators.required],
+            description: ['', Validators.required],
+        });
+        this.quantityAndPrice = this.formBuilder.group({
+            quantity: [1, [Validators.required, Validators.min(1)]],
+            price: [0, [Validators.required, Validators.min(0)]],
+        });
+        this.dates = this.formBuilder.group({
+            productionDate: [new Date(), Validators.required],
+            maxPickupDate: [new Date(), [Validators.required, Validators.min(Date.now())]],
+        });
+    }
 
-  get isFormValid() {
-    return this.nameAndDescription.valid && this.quantityAndPrice.valid && this.dates.valid;
-  }
+    get isFormValid() {
+        return this.nameAndDescription.valid && this.quantityAndPrice.valid && this.dates.valid;
+    }
 
-  addProduct() {
-    const newProduct: Product = {
-      name: this.nameAndDescription.controls.name.value,
-      description: this.nameAndDescription.controls.description.value,
-      quantityInitial: this.quantityAndPrice.controls.quantity.value,
-      quantityLeft: this.quantityAndPrice.controls.quantity.value,
-      price: this.quantityAndPrice.controls.price.value / 100,
-      imageURL: 'https://hips.hearstapps.com/hmg-prod/images/homemade-spaghetti-sauce-horizontal-1530890913.jpg',
-      maxPickupDate: new Date(this.dates.controls.maxPickupDate.value),
-      productionDate: new Date(this.dates.controls.productionDate.value),
-    };
-    this.productService.addProduct(newProduct).subscribe();
-  }
+    addProduct() {
+        const newProduct: Product = {
+            name: this.nameAndDescription.controls.name.value,
+            description: this.nameAndDescription.controls.description.value,
+            quantityInitial: this.quantityAndPrice.controls.quantity.value,
+            quantityLeft: this.quantityAndPrice.controls.quantity.value,
+            price: this.quantityAndPrice.controls.price.value / 100,
+            imageURL: 'https://hips.hearstapps.com/hmg-prod/images/homemade-spaghetti-sauce-horizontal-1530890913.jpg',
+            maxPickupDate: new Date(this.dates.controls.maxPickupDate.value),
+            productionDate: new Date(this.dates.controls.productionDate.value),
+        };
+        this.productService.addProduct(newProduct).subscribe();
+    }
 }
