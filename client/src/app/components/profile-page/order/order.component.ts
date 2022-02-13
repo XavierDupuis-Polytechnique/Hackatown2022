5226716e9c8bbadb685f72e8e2cad0603b3ccb2a
XavierDupuis <xavier0978@hotmail.fr>
2022-02-13 02:42:01 -0500

    does this commit even need a name anymore?

--- client/src/app/components/profile-page/order/order.component.ts
+++ client/src/app/components/profile-page/order/order.component.ts
@@ -1,7 +1,5 @@
 import { AfterViewInit, Component, Input } from '@angular/core';
 import { Order } from '@app/interfaces/order.interface';
-import { Product } from '@app/interfaces/product.interface';
-import { ProductService } from '@app/services/product-service/product.service';
 
 @Component({
   selector: 'app-order',
@@ -11,22 +9,9 @@ import { ProductService } from '@app/services/product-service/product.service';
 export class OrderComponent implements AfterViewInit {
 
   @Input() order: Order;
-  products: Product[] = []
 
-  constructor(private productService: ProductService) {
-
+  constructor(/*private productService: ProductService*/) {
   }
 
-  ngAfterViewInit(): void {
-    // for (const product of this.order.orderedProducts) {
-    //   this.productService.requestProduct(product.productId).subscribe((product) => {
-    //     console.log(product)
-    //   })
-    // }
-    // console.log(this.products)
-    const product = this.order.orderedProducts[0];
-    this.productService.requestProduct(product.productId).subscribe((product) => {
-      console.log(product);
-    });
-  }
+  ngAfterViewInit(): void {}
 }
