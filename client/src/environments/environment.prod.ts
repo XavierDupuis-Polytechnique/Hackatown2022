57850f74d2164f0d1e18e3c258a2e54dffecdaba
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2022-02-13 04:15:19 -0500

    https adding env client

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverURL: 'http://ec2-35-183-81-96.ca-central-1.compute.amazonaws.com:3000/api',
+    serverURL: 'https://ec2-35-183-81-96.ca-central-1.compute.amazonaws.com:8443/api',
 };
